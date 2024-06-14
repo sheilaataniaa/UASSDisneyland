b8ccd953df28a4e2266934d125ff59fa11bd93a6
kanepradipa <145975527+kanepradipa@users.noreply.github.com>
2024-06-14 13:31:07 +0700

    tinggal bagian bawah dan tombol dan semuanya kurang tombol kecuali giekert

--- lib/hpdisney.dart
+++ lib/hpdisney.dart
@@ -27,6 +27,7 @@ class Homepage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
+      
       children: [
         Container(
           width: 393,
@@ -308,195 +309,26 @@ class Homepage extends StatelessWidget {
 
 
               
-              Positioned(
-  left: 13,
-  top: 385,
-  child: Container(
-    width: 582,
-    height: 258,
-    child: Stack(
-      children: [
-
-        
-        Positioned(
-          left: 217, // Mengatur posisi horizontal
-          top: 0,    // Mengatur posisi vertikal
-          child: SizedBox(
-            width: 149,
-            height: 24,
-            child: Text(
-              'Special Deals!', // Mengubah teks
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                color: Colors.black,
-                fontSize: 24,
-                fontFamily: 'Abhaya Libre Medium',
-                fontWeight: FontWeight.w500,
-                height: 0.04,
-              ),
-            ),
-          ),
-        ),
 
 
-        Positioned(
-          left: 345,
-          top: 0,
-          child: Container(
-            width: 300,
-            height: 230,
-            decoration: ShapeDecoration(
-              gradient: LinearGradient(
-                begin: Alignment(0.00, -1.00),
-                end: Alignment(0, 1),
-                colors: [Color(0xFFBF6D32), Color(0xFFD5C366), Color(0xFF6E457C)],
-              ),
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10),
-              ),
-              shadows: [
-                BoxShadow(
-                  color: Color(0x3F000000),
-                  blurRadius: 4,
-                  offset: Offset(0, 4),
-                  spreadRadius: 0,
-                )
-              ],
-            ),
-          ),
-        ),
-        Positioned(
-          left: 359,
-          top: 80,
-          child: SizedBox(
-            width: 136,
-            child: Text(
-              'DISCOUNT 30%',
-              textAlign: TextAlign.justify,
-              style: TextStyle(
-                color: Colors.black,
-                fontSize: 16,
-                fontFamily: 'Abhaya Libre Medium',
-                fontWeight: FontWeight.w500,
-                height: 0.09,
-              ),
-            ),
-          ),
-        ),
-        Positioned(
-          left: 359,
-          top: 104,
-          child: SizedBox(
-            width: 262,
-            height: 91,
-            child: Text(
-              'Dapatkan diskon 30% untuk pembelian tiket DisneySea menggunakan aplikasi TMRW by UOB. Nikmati pengalaman seru di DisneySea dengan harga lebih hemat!',
-              textAlign: TextAlign.justify,
-              style: TextStyle(
-                color: Colors.black,
-                fontSize: 13,
-                fontFamily: 'Abhaya Libre Medium',
-                fontWeight: FontWeight.w500,
-                height: 1.4, // Sesuaikan nilai height dengan kebutuhan Anda
-              ),
-            ),
-          ),
-        ),
-        Positioned(
-          left: 359,
-          top: 16,
-          child: Container(
-            width: 197,
-            height: 46,
-            decoration: BoxDecoration(
-              image: DecorationImage(
-                image: AssetImage("images/tmrw.png"), // Ini benar
-                fit: BoxFit.fill,
-              ),
-            ),
-          ),
-        ),
-        Positioned(
-          left: 19,
-          top: 0,
-          child: Container(
-            width: 300,
-            height: 230,
-            decoration: ShapeDecoration(
-              gradient: LinearGradient(
-                begin: Alignment(0.00, -1.00),
-                end: Alignment(0, 1),
-                colors: [Color(0xFFBBBDCC), Color(0xFFB4BAC9), Color(0xFF45889D)],
-              ),
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10),
-              ),
-              shadows: [
-                BoxShadow(
-                  color: Color(0x3F000000),
-                  blurRadius: 4,
-                  offset: Offset(0, 4),
-                  spreadRadius: 0,
-                )
-              ],
-            ),
-          ),
-        ),
-        Positioned(
-          left: 34,
-          top: 100,
-          child: SizedBox(
-            width: 271,
-            height: 120,
-            child: Text(
-              'Nikmati penawaran spesial untuk pembelian tiket DisneySea dengan promo "Buy 1 Get 1" menggunakan aplikasi blu by BCA. Dengan promo ini, Anda bisa mendapatkan satu tiket gratis untuk setiap pembelian tiket DisneySea.',
-              textAlign: TextAlign.justify, // Atur textAlign menjadi justify
-              style: TextStyle(
-                color: Colors.black,
-                fontSize: 13,
-                fontFamily: 'Abhaya Libre Medium',
-                fontWeight: FontWeight.w500,
-                height: 1.5, // Sesuaikan nilai height dengan kebutuhan Anda
-              ),
-            ),
-          ),
-        ),
-        Positioned(
-          left: 33,
-          top: 80,
-          child: SizedBox(
-            width: 136,
-            child: Text(
-              'BUY 1 GET 1 FREE ',
-              textAlign: TextAlign.justify,
-              style: TextStyle(
-                color: Colors.black,
-                fontSize: 16,
-                fontFamily: 'Abhaya Libre Medium',
-                fontWeight: FontWeight.w500,
-                height: 0.09,
-              ),
-            ),
-          ),
-        ),
-        Positioned(
-          left: 0,
-          top: 9,
-          child: Container(
-            width: 153,
-            height: 70,
-            decoration: BoxDecoration(
-              image: DecorationImage(
-                image: AssetImage("images/blu.png"), // Ini benar
-                fit: BoxFit.fill,
-              ),
-            ),
-          ),
-        ),
-      ],
-    ),
-  ),
-),
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -504,69 +336,304 @@ class Homepage extends StatelessWidget {
 
 
               
-              Positioned(
-                left: 0,
-                top: 102,
+          Positioned(
+        left: 13,
+        top: 385,
+        child: Container(
+          width: 582,
+          height: 258,
+          child: PageView(
+            children: [
+
+              // Slide 1
+              Container(
+                child: Stack(
+                  children: [
+                    Positioned(
+                      left: 19,
+                      top: 0,
+                      child: Container(
+                        width: 300,
+                        height: 230,
+                        decoration: ShapeDecoration(
+                          gradient: LinearGradient(
+                            begin: Alignment(0.00, -1.00),
+                            end: Alignment(0, 1),
+                            colors: [
+                              Color(0xFFBBBDCC),
+                              Color(0xFFB4BAC9),
+                              Color(0xFF45889D)
+                            ],
+                          ),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          shadows: [
+                            BoxShadow(
+                              color: Color(0x3F000000),
+                              blurRadius: 4,
+                              offset: Offset(0, 4),
+                              spreadRadius: 0,
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                    Positioned(
+                      left: 33,
+                      top: 80,
+                      child: SizedBox(
+                        width: 136,
+                        child: Text(
+                          'BUY 1 GET 1 FREE',
+                          textAlign: TextAlign.justify,
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 16,
+                            fontFamily: 'Abhaya Libre Medium',
+                            fontWeight: FontWeight.w500,
+                            height: 0.09,
+                          ),
+                        ),
+                      ),
+                    ),
+                    Positioned(
+                      left: 34,
+                      top: 100,
+                      child: SizedBox(
+                        width: 271,
+                        height: 120,
+                        child: Text(
+                          'Nikmati penawaran spesial untuk pembelian tiket DisneySea dengan promo "Buy 1 Get 1" menggunakan aplikasi blu by BCA. Dengan promo ini, Anda bisa mendapatkan satu tiket gratis untuk setiap pembelian tiket DisneySea.',
+                          textAlign: TextAlign.justify,
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 13,
+                            fontFamily: 'Abhaya Libre Medium',
+                            fontWeight: FontWeight.w500,
+                            height: 1.5,
+                          ),
+                        ),
+                      ),
+                    ),
+                    Positioned(
+                      left: 0,
+                      top: 9,
+                      child: Container(
+                        width: 153,
+                        height: 70,
+                        decoration: BoxDecoration(
+                          image: DecorationImage(
+                            image: AssetImage("images/blu.png"),
+                            fit: BoxFit.fill,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+
+              // Slide 2
+              Container(
+                child: Stack(
+                  children: [
+                    Positioned(
+                      left: 345,
+                      top: 0,
+                      child: Container(
+                        width: 300,
+                        height: 230,
+                        decoration: ShapeDecoration(
+                          gradient: LinearGradient(
+                            begin: Alignment(0.00, -1.00),
+                            end: Alignment(0, 1),
+                            colors: [
+                              Color(0xFFBF6D32),
+                              Color(0xFFD5C366),
+                              Color(0xFF6E457C)
+                            ],
+                          ),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          shadows: [
+                            BoxShadow(
+                              color: Color(0x3F000000),
+                              blurRadius: 4,
+                              offset: Offset(0, 4),
+                              spreadRadius: 0,
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                    Positioned(
+                      left: 359,
+                      top: 80,
+                      child: SizedBox(
+                        width: 136,
+                        child: Text(
+                          'DISCOUNT 30%',
+                          textAlign: TextAlign.justify,
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 16,
+                            fontFamily: 'Abhaya Libre Medium',
+                            fontWeight: FontWeight.w500,
+                            height: 0.09,
+                          ),
+                        ),
+                      ),
+                    ),
+                    Positioned(
+                      left: 359,
+                      top: 104,
+                      child: SizedBox(
+                        width: 262,
+                        height: 91,
+                        child: Text(
+                          'Dapatkan diskon 30% untuk pembelian tiket DisneySea menggunakan aplikasi TMRW by UOB. Nikmati pengalaman seru di DisneySea dengan harga lebih hemat!',
+                          textAlign: TextAlign.justify,
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 13,
+                            fontFamily: 'Abhaya Libre Medium',
+                            fontWeight: FontWeight.w500,
+                            height: 1.4,
+                          ),
+                        ),
+                      ),
+                    ),
+                    Positioned(
+                      left: 359,
+                      top: 16,
+                      child: Container(
+                        width: 197,
+                        height: 46,
+                        decoration: BoxDecoration(
+                          image: DecorationImage(
+                            image: AssetImage("images/tmrw.png"),
+                            fit: BoxFit.fill,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+
+            ],
+          ),
+        ),
+
+
+      ),
+
+
+
+
+              
+            Positioned(
+        left: 0,
+        top: 102,
+        child: Container(
+          width: MediaQuery.of(context).size.width, // Full width of the screen
+          height: 229,
+          child: PageView(
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20.0), // Adjust horizontal padding
                 child: Container(
-                  width: 1188,
+                  width: MediaQuery.of(context).size.width - 40, // Full width minus padding
                   height: 229,
-                  child: Stack(
-                    children: [
-                      Positioned(
-                        left: 814.34,
-                        top: 0,
-                        child: Container(
-                          width: 373.66,
-                          height: 229,
-                          decoration: ShapeDecoration(
-                            image: DecorationImage(
-                              image: AssetImage("images/hp1.png"), // Ini benar
-                              fit: BoxFit.fill,
-                            ),
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(20),
-                            ),
-                          ),
-                        ),
-                      ),
-                      Positioned(
-                        left: 417.25,
-                        top: 0,
-                        child: Container(
-                          width: 376.35,
-                          height: 229,
-                          decoration: ShapeDecoration(
-                            image: DecorationImage(
-                              image: AssetImage("images/hp2.png"), // Ini benar
-                              fit: BoxFit.fill,
-                            ),
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(20),
-                            ),
-                          ),
-                        ),
-                      ),
-                      Positioned(
-                        left: 0,
-                        top: 0,
-                        child: Container(
-                          width: 396.08,
-                          height: 229,
-                          decoration: ShapeDecoration(
-                            image: DecorationImage(
-                              image: AssetImage("images/hp3.png"), // Ini benar
-                              fit: BoxFit.fill,
-                            ),
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(20),
-                            ),
-                          ),
-                        ),
-                      ),
-                    ],
+                  decoration: ShapeDecoration(
+                    image: DecorationImage(
+                      image: AssetImage("images/hp3.png"), // Ini benar
+                      fit: BoxFit.fill,
+                    ),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20),
+                    ),
                   ),
                 ),
               ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20.0), // Adjust horizontal padding
+                child: Container(
+                  width: MediaQuery.of(context).size.width - 40, // Full width minus padding
+                  height: 229,
+                  decoration: ShapeDecoration(
+                    image: DecorationImage(
+                      image: AssetImage("images/hp2.png"), // Ini benar
+                      fit: BoxFit.fill,
+                    ),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20.0), // Adjust horizontal padding
+                child: Container(
+                  width: MediaQuery.of(context).size.width - 40, // Full width minus padding
+                  height: 229,
+                  decoration: ShapeDecoration(
+                    image: DecorationImage(
+                      image: AssetImage("images/hp1.png"), // Ini benar
+                      fit: BoxFit.fill,
+                    ),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
